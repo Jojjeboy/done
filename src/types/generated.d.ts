150c7f60614444c3a973487ce50386038caab134
Johannes Kärnstam <karnstamjohannes@gmail.com>
2026-01-26 12:01:24 +0100

    More fixes

---
+++ src/types/generated.d.ts
@@ -0,0 +1,6 @@
+// Module declaration for generated commit info JSON
+declare module '@/generated/commit-info.json' {
+  import type { CommitData } from '@/types/commit-info'
+  const value: CommitData
+  export default value
+}
